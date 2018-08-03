305c35fcab17374797f486234f93e7f69c65c4b6
Ian Chan <ian.chan@weareadaptive.com>
2018-08-03 16:33:30 +0100

    major update

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import registerServiceWorker from './registerServiceWorker';
 import './App.css';
 import {connect, Provider} from 'react-redux';
 import thunkMiddleware from 'redux-thunk';
-import { createStore, applyMiddleware } from 'redux'
+import { createStore, applyMiddleware, combineReducers } from 'redux'
 import logger from 'redux-logger'
 import PropTypes from 'prop-types'
 import Plot from 'react-plotly.js'
@@ -24,11 +24,11 @@ const requestData = () => {
 }
 
 const receiveData = (json) => {
-  return {
-    type: "RECEIVE_DATA",
-    apiData: json,
-    receivedAt: Date()
-  }  
+    return {
+        type: "RECEIVE_DATA",
+        apiData: json,
+        receivedAt: Date()
+    }  
 }
 
 const receiveError = (error) => {
@@ -53,9 +53,13 @@ export const getData = (url) => {
         dispatch(requestData())
         return api.fetchApiData(url)
             .then((response) => {
+                console.debug(`@getData response`)
+                console.debug(response)
                 return response.json()
             })
             .then((json) => {
+                console.debug(`@getData parsed json`)
+                console.debug(json)
                 dispatch(receiveData(json))
             }).catch((error) => {
                 dispatch(receiveError(error))
@@ -63,67 +67,116 @@ export const getData = (url) => {
     }
 }
 
-
-const connected = () => {
+//web socket
+const wsConnect = (url, channel) => {
     return {
-        type: "Connected"
+        type: "WEBSOCKET_CONNECT",
+        url,
+        channel
     }
 }
 
-const disconnected = () => {
+const wsConnected = (event) => {
     return {
-        type: "Disconnected"
+        type: "WEBSOCKET_CONNECTED",
+        eventType: event.type,
+        connectedAt: Date()
     }
 }
 
-const receiveMessage = (msg) => {
+const wsSubscribe = (channel) => {
     return {
-        type: "RECEIVE_MESSAGE",
-        msgr,
+        type: "WEBSOCKET_SUBSCRIBE",
+        channel 
     }
 }
 
-const webSocketApi =  {
-    websocket : (url) => {
-        return new WebSocket(url)
+const wsUnsubscribe = () => {
+    return {
+        type: "WEBSOCKET_UNSUBSCRIBE",
     }
+}
 
+const wsDisconnecting = () => {
+    return {
+        type: "WEBSOCKET_DISCONNECTING",
+    }
+}
+
+const wsDisconnected = (event) => {
+    return {
+        type: "WEBSOCKET_DISCONNECTED",
+        eventType: event.type,
+        disconnectedAt: Date()
+    }
+}
+
+const wsReceiveMessage = (message) => {
+    console.log(message)
+    return {
+        type: "WEBSOCKET_RECEIVE_MESSAGE",
+        message
+    }
+}
+
+const wsError = (event) => {
+    return {
+        type: 'WEBSOCKET_ERROR',
+        eventType: event.type
+    }
 }
 
 // Create WebSocket connection.
-export const connectBlockChainWebSocket = (url) => {
-    webSpcketApi.websocket(url)    
-    websocket.onopen = () => onOpen()
-    websocket.onmessage = (event) => onMessage(event)
-    websocket.onerror = (event) => onError(event)
-    websocket.onclose = (event) => onClose(event)
-    https://exec64.co.uk/blog/websockets_with_redux/
-    // function onOpen() {
-    //     // sendMessage({
-    //     //     "op":"unconfirmed_sub"
-    //     // });
-    //     console.log("open")
-    // }
+// The goal of our WebSocket middleware is to allow bi-directional access to a WebSocket connection through the dispatch of actions. 
+export const connectBlockchainWebSocketMiddleware = (store) => (next) => (action) => {
+    let websocket = null
+    const onOpen = (socket, store, channel) => (event) => {
+        store.dispatch(wsConnected(event))
+        //send message to subscribe to the websocket
+        sendMessage(socket, channel)
+    }
+    
+    const onMessage = (store) => (event) => {
+        store.dispatch(wsReceiveMessage(event))
+    }
+    
+    const onClose = (store) => (event) =>{
+        store.dispatch(wsDisconnected(event))
+    }
+    
+    const onError = (store) => (event) => {
+        store.dispatch(wsError(event))
+    }
+    
+    const sendMessage = (socket, channel) => {
+        if(channel === "ticker") {
+            store.dispatch(wsSubscribe(channel))
+            socket.send(JSON.stringify({"channel":channel}))
+        } else {
+            store.dispatch(wsUnsubscribe())
+            socket.send(JSON.stringify({"op":"unconfirmed_unsub"}))
+        }
+    }
 
-    // function onMessage(event) {
-    //     console.log(JSON.parse(event.data))
-    // }
-
-    // function onClose() {
-    //     sendMessage({
-    //         "op":"unconfirmed_unsub"
-    //     });
-    // }
-
-    // function sendMessage(msg) {
-    //     websocket.send(JSON.stringify(msg))
-    // }
-
-    // function onError(event) {
-    //     console.log(event)
-    // }
+    // User Dispatched Actions
+    switch(action.type) {
+        case "WEBSOCKET_CONNECT":
+            //connect here
+            websocket = new WebSocket(action.url)
+            websocket.onopen = onOpen(websocket, store, action.channel)
+            websocket.onmessage = onMessage(store)
+            websocket.onclose = onClose(store)
+            websocket.onerror = onError(store)
+            break
+        case "WEBSOCKET_DISCONNECT":
+            store.dispatch(wsDisconnecting())
+            websocket.close()
+            break
+        default:
+            break
+    }
+    return next(action);   
 }
-
 
 // Reducers - specify how the application's state changes in response to actions sent to store
 // All the application state is stored as a single object
@@ -133,7 +186,7 @@ export const connectBlockChainWebSocket = (url) => {
 // initial state
 const dataInitialState = {
     isFetching: false,
-    apiData: {},
+    apiData: "",
     lastUpdated: "",
     error: ""
 }
@@ -161,24 +214,70 @@ export const dataReducer = (state = dataInitialState, action) => {
 }
 
 const blockchainInitialState = {
-    msg: {},
-    error: ""
+    wsConnected: false,
+    wsStatus: "close",
+    channel: "",
+    message: "",
+    eventTime: "",
+    blockchainError: ""
 }
 
 const blockchainReducer = (state = blockchainInitialState, action) => {
-    switch(active.type) {
-        case "CONNECTED":
+    switch(action.type) {
+        case "WEBSOCKET_CONNECT":
             return Object.assign({}, state, {
-                msg: "Welcome"
+                wsConnected: false,
+                wsStatus: "connecting",
+                channel: action.channel
             })
+        case "WEBSOCKET_CONNECTED":
+            return Object.assign({}, state, {
+                wsConnected: true,
+                wsStatus: action.eventType,
+                eventTime: action.connectedAt
+            })
+        case "WEBSOCKET_SUBSCRIBE":
+            return Object.assign({}, state, {
+                wsConnected: true,
+                channel: action.channel
+            })
+        case "WEBSOCKET_RECEIVE_MESSAGE":
+            return Object.assign({}, state, {
+                wsConnected: true,
+                wsStatus: action.message.type,
+                message: action.message.data
+            })  
+        case "WEBSOCKET_DISCONNECTING":
+            return Object.assign({}, state, {
+                wsConnected: true,
+            })
+        case "WEBSOCKET_DISCONNECTED":
+            return Object.assign({}, state, {
+                wsConnected: false,
+                wsStatus: action.eventType,
+                eventTime: action.disconnectedAt
+            })
+        case "WEBSOCKET_ERROR":
+            return Object.assign({}, state, {
+                wsConnected: false,
+                wsStatus: action.eventType,
+                channel: ""
+            })
+        default:
+            return state
     }
 }
+
+const rootReducer = combineReducers({
+    dataReducer,
+    blockchainReducer
+})
 
 // Store - An object that holds the complete state of your app.
 // Pass two arguments to the reducer, current state tree and the action.
 // Creates a Redux store that holds the complete state tree of your app.
 const store = createStore(
-    dataReducer,
+    rootReducer,
     /* 
         The key feature of middleware is that it is composable. Multiple middleware can be combined together,
         where each middleware requires no knowledge of what comes before or after it in the chain.
@@ -191,12 +290,13 @@ const store = createStore(
             It provides a third-party extension point between dispatching an action, 
             and the moment it reaches the reducer.
         */
+        logger,
         thunkMiddleware.withExtraArgument({api}), // They would receive dispatch as an argument and may call it asynchronously. 
         /*
             It is a middleware that looks at every action that passes through the system, and if it’s a function,
             it calls that function. That’s all it does.
         */
-        logger
+        connectBlockchainWebSocketMiddleware
     )
 )
 
@@ -207,33 +307,58 @@ const store = createStore(
 */
 
 class App extends Component {
-    componentDidMount() {
-        const url = `https://api.coindesk.com/v1/bpi/historical/close.json`
-        this.props.fetchData(url)
+    constructor(props) {
+        super(props)
+        this.state = {date: new Date()}
     }
 
+    componentDidMount() {
+        this.timer = setInterval(() => this.tick(), 1000)
+        const url = `https://api.coindesk.com/v1/bpi/historical/close.json`
+        this.props.fetchData(url)
+
+        const wsUrl = `ws://localhost:4000`
+        const channel = "ticker"
+        this.props.startWebSocket(wsUrl, channel)
+    }
+
+    tick() {
+        this.setState({date: new Date()})
+    }
+    
     render() {
-
-
-        const fetchState = this.props.isFetching
+        // From Historial Price API
+        const apiFetchState = this.props.isFetching
         const {bpi, time} = this.props.apiData
         const updateTime = this.props.lastUpdated
-        const error = this.props.error
+        const apiError = this.props.error
         
+        // BlockChain WebSocket Data
+        const wsConnected = this.props.wsConnected
+        const wsStatus = this.props.wsStatus
+        const channel = this.props.channel
+        const wsData = this.props.message
+        const eventTime = this.props.eventTime
+        const blockchainError = this.props.blockchainError
+
         return (
             <div className="container">
-                {fetchState &&
-                    <h4>Loading</h4>
-                }
-                {time &&
-                    <Header serverUpdate={time} update={updateTime}/>
-                }
-                {error &&
-                    <p>{error.message}</p>
-                }
-                {bpi && 
+                <Header localTime={this.state.date} apiServerUpdateTime={time} fetchTime={updateTime} apiFetchState={apiFetchState} 
+                        wsConnectState={wsConnected} wsStatus={wsStatus} wsConnectionTime={eventTime} channel={channel}
+                />
+
+                {wsConnected && !blockchainError &&  wsData ? (
+                    <BlockChainData wsData={wsData} />
+                ) : (
+                    <p>{blockchainError.message}</p>
+                )}
+                                
+                {bpi && !apiError && !apiFetchState ? (
                     <DisplayBox data={bpi} /> 
-                }
+                ) : (
+                    <p>{apiError.message}</p>
+                )}
+
                 <Footer />
             </div>
         )
@@ -244,6 +369,39 @@ App.propTypes = {
     bpi: PropTypes.object,
     time: PropTypes.object,
     updateTime: PropTypes.string
+}
+
+class BlockChainData extends Component {
+    render() {
+        const parsedData = JSON.parse(this.props.wsData)
+        
+        return (
+            <div>
+                <p><b>Trade Pair</b> {parsedData.pair}</p>
+                <p><b>Volume</b> {parsedData.volume}</p>
+                <table>
+                    <thead>
+                        <tr>
+                            <th>Bid</th>
+                            <th>Ask</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td>
+                                <div onChange={console.log(parsedData.bid)}>
+                                    {parsedData.bid}
+                                </div>
+                            </td>
+                            <td>
+                                {parsedData.ask}
+                            </td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
+        )
+    }
 }
 
 class DisplayBox extends Component {
@@ -299,8 +457,33 @@ class Header extends Component {
     render() {
         return (
             <header>
-                <h2>Data Updated At: {Object.values(this.props.serverUpdate)[0]}</h2>
-                <h2>Local Time: {this.props.update} </h2>
+                <p><b>Local Time: </b>{this.props.localTime.toLocaleTimeString()}</p>
+                
+                {this.props.apiServerUpdateTime ? (
+                    <div>
+                        <p><b>API Data Updated At: </b> {Object.values(this.props.apiServerUpdateTime)[0]}</p>
+                        <p><b>Data Fetch At: </b> {this.props.fetchTime}</p> 
+                    </div>
+                ) : (
+                    <div>
+                        <p><b>Price API is Fetching:</b> {(this.props.apiFetchState).toString()}</p>
+                    </div>
+                )}
+
+                {this.props.wsConnectState ? (
+                    <div>
+                        <p><b>WebSocket Connected:</b> {(this.props.wsConnectState).toString()}</p>
+                        <p><b>WebSocket Status:</b> {this.props.wsStatus}</p>
+                        <p><b>Channel:</b> {this.props.channel}</p>
+                        <p><b>WebSocket Connected At:</b> {this.props.wsConnectionTime}</p>
+                    </div>
+                ) : (
+                    <div>
+                        <p><b>WebSocket Connected:</b> {(this.props.wsConnectState).toString()}</p>
+                        <p><b>WebSocket Status:</b> {this.props.wsStatus}</p>
+                        <p><b>WebSocket Disconnected At:</b> {this.props.wsConnectionTime}</p>
+                    </div>
+                )}
             </header>
         )
     }
@@ -310,19 +493,6 @@ class Footer extends Component {
     render() {
         return (
             <footer>
-                <p>Blockchain Movement</p>
-                <table>
-                    <thead>
-                        <tr>
-                            <th>In Address</th>
-                            <th>Out Address</th>
-                        </tr>
-                        <tr>
-                            <td>In Address</td>
-                            <td>Out Address</td>
-                        </tr>
-                    </thead>
-                </table>
                 <h4>Best App</h4>
             </footer>
         )
@@ -331,12 +501,35 @@ class Footer extends Component {
 
 // Tells how to transform the current Redux store state into the props you want to pass to a presentational component you are wrapping.
 // If this argument is specified, the new component will subscribe to Redux store updates. This means that any time the store is updated, mapStateToProps will be called. 
-const mapStateToProps = (state) => ({
-    isFetching: state.isFetching,
-    apiData: state.apiData,
-    lastUpdated: state.lastUpdated,
-    error: state.error
-});
+const mapStateToProps = (state) => {
+    const {dataReducer, blockchainReducer} = state
+    const {
+        isFetching,
+        apiData,
+        lastUpdated,
+        error
+    } = dataReducer
+    const {
+        wsConnected,
+        wsStatus,
+        channel,
+        message,
+        eventTime,
+        blockchainError
+    } = blockchainReducer
+    return {
+        isFetching,
+        apiData,
+        lastUpdated,
+        error,
+        wsConnected,
+        wsStatus,
+        channel,
+        message,
+        eventTime,
+        blockchainError
+    }
+}
 
 // If a function is passed, it will be given dispatch as the first parameter
 
@@ -345,7 +538,10 @@ const mapStateToProps = (state) => ({
 // It returns a new, connected component class for you to use
 const ConnectedApp = connect(
     mapStateToProps,
-    {fetchData: getData}
+    {
+        fetchData: getData,
+        startWebSocket: wsConnect
+    }
 )(App)
 
 // Proptypes- runtime checking for React props, to document the intended types of properties passed to components. 
